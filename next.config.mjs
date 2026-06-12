b82d37a59eaf4723a493188abaf235f0ef8aa4dc
Cristian Alberto Saez Morales <cristian.saez@uautonoma.cl>
2026-06-12 16:37:48 -0400

    fix: pin older CJS versions of merge-deep, clone-deep, and is-plain-object and add to external packages for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium-min', 'puppeteer', 'puppeteer-extra', 'puppeteer-extra-plugin-stealth'],
+        serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium-min', 'puppeteer', 'puppeteer-extra', 'puppeteer-extra-plugin-stealth', 'is-plain-object', 'clone-deep', 'merge-deep'],
     },
 };
 
