70274b7a9bc88fd516d9bf8ed40a026324a5c51c
Cristian Alberto Saez Morales <cristian.saez@uautonoma.cl>
2026-06-15 09:44:10 -0400

    fix: restore explicit outputFileTracingIncludes because Turbopack dead-code-elimination stripped the static require statements

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,35 @@ const nextConfig = {
     serverExternalPackages: [
         'puppeteer-core', '@sparticuz/chromium-min', 'puppeteer', 'puppeteer-extra', 'puppeteer-extra-plugin-stealth', 
         'merge-deep', 'clone-deep', 'is-plain-object', 'isobject', 'for-own', 'kind-of', 'shallow-clone', 'for-in', 'mixin-object', 'lazy-cache', 'is-extendable',
-        'puppeteer-extra-plugin-user-preferences', 'puppeteer-extra-plugin-user-data-dir', 'puppeteer-extra-plugin', 'deepmerge'
-    ]
+        'puppeteer-extra-plugin-user-preferences', 'puppeteer-extra-plugin-user-data-dir', 'puppeteer-extra-plugin', 'deepmerge', 'fs-extra'
+    ],
+    experimental: {
+        outputFileTracingIncludes: {
+            '/api/**/*': [
+                './node_modules/puppeteer-extra/**/*',
+                './node_modules/puppeteer-extra-plugin-stealth/**/*',
+                './node_modules/puppeteer-extra-plugin-user-preferences/**/*',
+                './node_modules/puppeteer-extra-plugin-user-data-dir/**/*',
+                './node_modules/puppeteer-extra-plugin/**/*',
+                './node_modules/deepmerge/**/*',
+                './node_modules/fs-extra/**/*',
+                './node_modules/rimraf/**/*',
+                './node_modules/graceful-fs/**/*',
+                './node_modules/jsonfile/**/*',
+                './node_modules/universalify/**/*',
+                './node_modules/merge-deep/**/*',
+                './node_modules/clone-deep/**/*',
+                './node_modules/is-plain-object/**/*',
+                './node_modules/isobject/**/*',
+                './node_modules/kind-of/**/*',
+                './node_modules/shallow-clone/**/*',
+                './node_modules/for-own/**/*',
+                './node_modules/for-in/**/*',
+                './node_modules/mixin-object/**/*',
+                './node_modules/lazy-cache/**/*',
+                './node_modules/is-extendable/**/*'
+            ]
+        }
+    }
 };
 export default nextConfig;
