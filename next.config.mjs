44824987af50dceafecf9bd521a4a47a0f14aef3
Cristian Alberto Saez Morales <cristian.saez@uautonoma.cl>
2026-06-15 09:03:46 -0400

    fix: trace all explicitly flattened legacy modules to force vercel copy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium-min', 'puppeteer', 'puppeteer-extra', 'puppeteer-extra-plugin-stealth', 'merge-deep', 'clone-deep', 'is-plain-object', 'isobject', 'for-own', 'kind-of', 'shallow-clone', 'for-in', 'mixin-object'],
+    serverExternalPackages: [
+        'puppeteer-core', '@sparticuz/chromium-min', 'puppeteer', 'puppeteer-extra', 'puppeteer-extra-plugin-stealth', 
+        'merge-deep', 'clone-deep', 'is-plain-object', 'isobject', 'for-own', 'kind-of', 'shallow-clone', 'for-in', 'mixin-object', 'lazy-cache', 'is-extendable'
+    ],
+    experimental: {
+        outputFileTracingIncludes: {
+            '/api/**/*': [
+                './node_modules/puppeteer-extra/**/*',
+                './node_modules/puppeteer-extra-plugin-stealth/**/*',
+                './node_modules/merge-deep/**/*',
+                './node_modules/clone-deep/**/*',
+                './node_modules/is-plain-object/**/*',
+                './node_modules/isobject/**/*',
+                './node_modules/kind-of/**/*',
+                './node_modules/shallow-clone/**/*',
+                './node_modules/for-own/**/*',
+                './node_modules/for-in/**/*',
+                './node_modules/mixin-object/**/*',
+                './node_modules/lazy-cache/**/*',
+                './node_modules/is-extendable/**/*'
+            ]
+        }
+    }
 };
 
 export default nextConfig;
